0750f90dd76c5861988a94196afae57bee261ce8
Silviu Matei <tzevy.cont@gmail.com>
2023-02-26 21:51:40 +0200

    github automate build webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./src/index.html",
+                    to: `${__dirname}/dist/index.html`,
+                },
+                {
+                    from: "./src/style.css",
+                    to: `${__dirname}/dist/style.css`,
+                },
+            ],
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new HtmlMinimizerPlugin(),
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ],
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "main.js",
+    },
+    mode: "production",
+    module: {},
+};
